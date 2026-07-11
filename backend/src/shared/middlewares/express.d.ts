ad291661a8efb6d0700d87ff29bfe054b2f55cac
Juan <juandiego.velez@utp.edu.co>
2026-07-11 09:15:42 -0500

    log: agregar log al tipo de express global

--- backend/src/shared/middlewares/express.d.ts
+++ backend/src/shared/middlewares/express.d.ts
@@ -1,10 +1,15 @@
-declare module 'express' {
-  interface Request {
-    user?: {
-      id: string;
-      email: string;
-      role: string | null;
-    };
+import type { Logger } from 'pino';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: string | null;
+      };
+      log: Logger;
+    }
   }
 }
 
